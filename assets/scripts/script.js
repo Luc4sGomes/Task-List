4ef8f0954f70ab03a53b703dc1de357d1ec59634
lucas <lucasgominhaa448@gmail.com>
2021-05-26 15:18:19 -0300

    added new changes in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,8 @@
 const inputTask = document.querySelector('.input-task');
 const buttonTask = document.querySelector('.btn-task');
 const tasks = document.querySelector('tasks');
+
+
+buttonTask.addEventListener('click',function(event){
+
+});
