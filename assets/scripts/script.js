da28ccb9f5cba6c15d11952e1721f85950241a63
lucas <lucasgominhaa448@gmail.com>
2021-05-26 16:56:10 -0300

    added new comments in style.css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,18 @@
-const inputTask = document.querySelector(".input-task");
+const inputTask = document.querySelector(".input-task"); 
 const buttonTask = document.querySelector(".btn-task");
 const tasks = document.querySelector(".tasks");
 
 inputTask.addEventListener("keypress", function (event) {
   if (event.keyCode === 13) {
     //verify the key of enter
-    if (!inputTask) return; //if is empty
+    if (!inputTask) return; //if input is empty
 
     addTask(inputTask.value); //else
     cleanInput();
   }
 });
 
-buttonTask.addEventListener("click", function () {
-  //eventButton
+buttonTask.addEventListener("click", function () {  //eventButton
 
   if (!inputTask.value) return;
 
@@ -21,7 +20,7 @@ buttonTask.addEventListener("click", function () {
   cleanInput();
 });
 
-function createButtonRemove(li) {
+function createButtonRemove(li) {  //create the button to remove next to the tasks
   li.innerText += " ";
 
   const buttonRemove = document.createElement("button");
@@ -30,12 +29,12 @@ function createButtonRemove(li) {
   li.appendChild(buttonRemove);
 }
 
-function cleanInput() {
+function cleanInput() { //clean the input and add the focus
   inputTask.value = "";
   inputTask.focus();
 }
 
-function addTask(inputText) {
+function addTask(inputText) { //create 1 task
   const li = createLi();
   li.innerHTML = inputText;
   tasks.appendChild(li);
@@ -43,20 +42,20 @@ function addTask(inputText) {
   saveTasks();
 }
 
-function createLi() {
+function createLi() { //createa 1 li
   const li = document.createElement("li");
   return li;
 }
 
-document.addEventListener("click", function (event) {
-  const element = event.target;
+document.addEventListener("click", function (event) {  //verify if element contains the class remove(53) and remove
+  const element = event.target; 
 
-  if (element.classList.contains("remove")) {
-    element.parentElement.remove();
+  if (element.classList.contains("remove")) {  //(53)
+    element.parentElement.remove(); //remove the element in case the tasks created
   }
 });
 
 
 function saveTasks(){
-
+  const 
 }
