41f9092609f5959fd7ad08fb2b752c6439d3cf7f
lucas <lucasgominhaa448@gmail.com>
2021-05-26 16:42:16 -0300

    added changes in global.css and script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,13 @@
-const inputTask = document.querySelector(".input-task"); 
+const inputTask = document.querySelector(".input-task");
 const buttonTask = document.querySelector(".btn-task");
 const tasks = document.querySelector(".tasks");
 
-inputTask.addEventListener("keypress", function (event) { 
-  if (event.keyCode === 13) { //verify the key of enter
+inputTask.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    //verify the key of enter
     if (!inputTask) return; //if is empty
 
-    addTask(inputTask.value); //else 
+    addTask(inputTask.value); //else
     cleanInput();
   }
 });
@@ -20,18 +21,42 @@ buttonTask.addEventListener("click", function () {
   cleanInput();
 });
 
+function createButtonRemove(li) {
+  li.innerText += " ";
+
+  const buttonRemove = document.createElement("button");
+  buttonRemove.setAttribute("class", "remove");
+  buttonRemove.innerText = "Remove";
+  li.appendChild(buttonRemove);
+}
+
 function cleanInput() {
   inputTask.value = "";
   inputTask.focus();
 }
 
-function addTask(inputText) { 
+function addTask(inputText) {
   const li = createLi();
   li.innerHTML = inputText;
   tasks.appendChild(li);
+  createButtonRemove(li);
+  saveTasks();
 }
 
 function createLi() {
   const li = document.createElement("li");
   return li;
+}
+
+document.addEventListener("click", function (event) {
+  const element = event.target;
+
+  if (element.classList.contains("remove")) {
+    element.parentElement.remove();
+  }
+});
+
+
+function saveTasks(){
+
 }
